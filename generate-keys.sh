48c635d9ff620f6a8313a8bea31966770cb7d4d7
Alex Suraci <suraci.alex@gmail.com>
2016-07-21 17:28:19 -0700

    fix key generation

--- generate-keys.sh
+++ generate-keys.sh
@@ -2,10 +2,12 @@
 
 set -e -u -x
 
-ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
-ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+mkdir -p keys/web keys/worker
 
-ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+yes | ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+yes | ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+
+yes | ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
 
 cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
 cp ./keys/web/tsa_host_key.pub ./keys/worker
