486894e6d6f84aad112c14094bca18bec8c48154
Jamie Klassen <cklassen@pivotal.io>
2020-04-23 16:44:59 -0400

    do not override key paths if already set
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-CONCOURSE_SESSION_SIGNING_KEY=/concourse-keys/session_signing_key
+CONCOURSE_SESSION_SIGNING_KEY=${CONCOURSE_SESSION_SIGNING_KEY:-/concourse-keys/session_signing_key}
 [ -f $CONCOURSE_SESSION_SIGNING_KEY ] && export CONCOURSE_SESSION_SIGNING_KEY
-CONCOURSE_TSA_AUTHORIZED_KEYS=/concourse-keys/authorized_worker_keys
+CONCOURSE_TSA_AUTHORIZED_KEYS=${CONCOURSE_TSA_AUTHORIZED_KEYS:-/concourse-keys/authorized_worker_keys}
 [ -f $CONCOURSE_TSA_AUTHORIZED_KEYS ] && export CONCOURSE_TSA_AUTHORIZED_KEYS
-CONCOURSE_TSA_HOST_KEY=/concourse-keys/tsa_host_key
+CONCOURSE_TSA_HOST_KEY=${CONCOURSE_TSA_HOST_KEY:-/concourse-keys/tsa_host_key}
 [ -f $CONCOURSE_TSA_HOST_KEY ] && export CONCOURSE_TSA_HOST_KEY
-CONCOURSE_TSA_PUBLIC_KEY=/concourse-keys/tsa_host_key.pub
+CONCOURSE_TSA_PUBLIC_KEY=${CONCOURSE_TSA_PUBLIC_KEY:-/concourse-keys/tsa_host_key.pub}
 [ -f $CONCOURSE_TSA_PUBLIC_KEY ] && export CONCOURSE_TSA_PUBLIC_KEY
-CONCOURSE_TSA_WORKER_PRIVATE_KEY=/concourse-keys/worker_key
+CONCOURSE_TSA_WORKER_PRIVATE_KEY=${CONCOURSE_TSA_WORKER_PRIVATE_KEY:-/concourse-keys/worker_key}
 [ -f $CONCOURSE_TSA_WORKER_PRIVATE_KEY ] && export CONCOURSE_TSA_WORKER_PRIVATE_KEY
 exec /usr/local/concourse/bin/concourse $@
